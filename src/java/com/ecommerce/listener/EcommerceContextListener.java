7bde12cbd52e77cfe922d019ddece24c36082e3e
Mohamed Gamal <mohamed.gamal987.mg@gmail.com>
2018-09-16 01:35:51 +0200

    get all categories and add it to context when app is created to use in nav bar

--- src/java/com/ecommerce/listener/EcommerceContextListener.java
+++ src/java/com/ecommerce/listener/EcommerceContextListener.java
@@ -5,7 +5,10 @@
  */
 package com.ecommerce.listener;
 
+import com.ecommerce.bean.Category;
+import com.ecommerce.dao.CategoryDaoImpl;
 import com.ecommerce.helper.MySQLDatabaseHelper;
+import java.util.List;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -13,32 +16,36 @@ import javax.servlet.ServletContextListener;
 public class EcommerceContextListener implements ServletContextListener {
 
     /**
-     * create database connection and set it to the app context
-     * when app created for the first time
+     * create database connection and set it to the app context when app created
+     * for the first time
      *
-     * @param sce 
+     * @param sce
      */
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext sc = sce.getServletContext();
- 
+
         // get database init params from app context  
-    	String dbUrl = sc.getInitParameter("dbUrl");
-    	String dbUsername = sc.getInitParameter("dbUsername");
-    	String dbPassword = sc.getInitParameter("dbPassword");
-    	String dbName = sc.getInitParameter("dbName");
-        
+        String dbUrl = sc.getInitParameter("dbUrl");
+        String dbUsername = sc.getInitParameter("dbUsername");
+        String dbPassword = sc.getInitParameter("dbPassword");
+        String dbName = sc.getInitParameter("dbName");
+
         // get database singletone instance
-    	MySQLDatabaseHelper db = MySQLDatabaseHelper.getInstance(dbUrl + dbName, dbUsername, dbPassword);
-        
+        MySQLDatabaseHelper db = MySQLDatabaseHelper.getInstance(dbUrl + dbName, dbUsername, dbPassword);
+
         // set database to request
         sc.setAttribute("db", db);
-        }
+        
+        List<Category> categories = new CategoryDaoImpl(sc).getAllCategories("ASC");
+        
+        sc.setAttribute("navCategories", categories);
+    }
 
     /**
      * close database connection when app destroyed
-     * 
-     * @param sce 
+     *
+     * @param sce
      */
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
